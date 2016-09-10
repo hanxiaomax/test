5bf4a5a901e2166059d6f0829fd4acd506d6f6a1
hanxiaomax-mac <hanxiaomax@qq.com>
2016-09-10 21:53:39 +0800

    hot fix

--- ShanBayTest/settings.py
+++ ShanBayTest/settings.py
@@ -80,7 +80,7 @@ ONLINE=True
 
 name = "rewordsDB"
 user = "root"
-password = ""
+password = "qwe"
 host = ""
 port = ""
 
@@ -177,4 +177,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_URL = '/static/'  
 
-#STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
